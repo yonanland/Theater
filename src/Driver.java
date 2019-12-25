fdd6d4e46b1c0638ea9575df8c5620a7c3ccce20
gutama8787 <gutama8787@gmail.com>
2019-12-25 16:56:00 -0500

    clean up

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,6 @@
 
 public class Driver {
-
+	public static void main(String[] args) {
+		System.out.println("hello word!");
+	}
 }
--- src/snippet/Snippet.java
+++
@@ -1,9 +0,0 @@
-package snippet;
-
-public class Snippet {
-	
-	
-	Gutama@DESKTOP-FLHA423 MINGW64 ~/eclipse-workspace/Theater (master)
-	$
-}
-
