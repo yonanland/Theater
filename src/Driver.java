6f2e2866767de877c1dc591ad2408eaeca701601
Yonas Abraha <59180135+yonanland@users.noreply.github.com>
2019-12-30 15:10:06 -0500

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,8 @@
 
-public class Driver {
-	public static void main(String[] args) {
-		System.out.println("hello word!");
+public class Driver 
+{
+	public static void main(String[] args) 
+        {
+		System.out.println("Hello guys!\nHappyNew year 2020!!");
 	}
 }
